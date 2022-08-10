38a0906275e38fb408b36dd027295bf041413f0b
Jeff Whitehead <jwhitehead@foreflight.com>
2022-08-10 09:22:29 -0500

    generate airport data in card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,9 +3,10 @@ import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-import api from '../../api/posts';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
+import axios from "axios";
 
+const baseURL = "http://localhost:3000/airports/50r.json"
 
 // useEffect(() => {
 //   const fetchPosts = async() => {
@@ -39,17 +40,33 @@ const useStyles = makeStyles({
 export default function SimpleCard() {
 
 
- const [posts, setPosts] = useState([]);
+ const [posts, setPosts] = React.useState([]);
+
+ React.useEffect(() => {
+  axios.get(baseURL).then((response) => {
+    setPosts(response.data)
+    console.log(response.data)
+    console.log(posts);
+  });
+ }, []);
   
   const classes = useStyles();
 
 
   return (
-    <Card className={classes.root}>
+    <div>
+    <h4>ICAO: {posts.icao}</h4>
+    <h4>Name: {posts.name}</h4>
+    {/* <h4>Available Runways: {posts.runways[0].ident}</h4> */}
+    <h4>Latitude: {posts.latitude}</h4>
+    <h4>Longitude: {posts.longitude}</h4>
+
+    {/* <Card className={classes.root}>
       <CardContent>
         <h2>Airport</h2>
+        <h3>ICAO:{posts.name}</h3>
         <Typography className={classes.title}  gutterBottom>
-          ICAO: 
+          ICAO:
         </Typography>
         <Typography className={classes.title}  gutterBottom>
           Airport Name:  
@@ -59,7 +76,8 @@ export default function SimpleCard() {
         </Typography>
       </CardContent>
      
-    </Card>
+    </Card> */}
+    </div>
   );
 }
 
